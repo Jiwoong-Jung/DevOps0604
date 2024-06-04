89ba7f4f85d741efcf81adef78b1a6ec2db82b01
Jiwoong Jung <jjw981119@naver.com>
2024-06-04 09:53:20 +0900

    first commit

---
+++ vscSB/sb0604/src/main/java/com/sky/sb0604/Sb0604Application.java
@@ -0,0 +1,13 @@
+package com.sky.sb0604;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb0604Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb0604Application.class, args);
+	}
+
+}
---
+++ vscSB/sb0604/src/test/java/com/sky/sb0604/Sb0604ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.sb0604;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb0604ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
